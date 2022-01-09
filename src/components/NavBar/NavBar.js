ce28dace2e5c559c8c977820d2bdefc3d402e432
TbanRam <esteban.ramallo@hotmail.com>
2022-01-09 20:00:42 -0300

    se agrega menu

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,10 @@
+function NavBar() {
+    return( <div>
+    <button style={{color: "rgb(255, 99, 99)"}} onClick>Home</button>
+    <button style={{color: "rgb(255, 99, 99)"}} onClick>Sillas</button>
+    <button style={{color: "rgb(255, 99, 99)"}} onClick>Mesas</button>
+    <button style={{color: "rgb(255, 99, 99)"}} onClick>Placards</button>
+    <button style={{color: "rgb(255, 99, 99)"}} onClick>Contacto</button>
+    </div>)
+}
+export default NavBar;
